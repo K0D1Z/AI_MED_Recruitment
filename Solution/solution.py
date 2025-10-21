60febff7d869cd575dd15f72ae73713f7ebd1bb4
Konrad Zatorski <kodiz2005@gmail.com>
2025-10-21 20:40:47 +0200

    Data loaded from the task_data.csv file and converted to proper types. Data splitted into labels and samples.

---
+++ Solution/solution.py
@@ -0,0 +1,47 @@
+import csv
+
+FILENAME = "task_data.csv"
+TARGET_COLUMN = "Cardiomegaly"
+
+# Some strings contain comma instead of dot
+def convert_comma_separated_number(number):
+    number = number.replace(",", ".")
+    return float(number)
+
+X_data = [] # List of samples
+y_data = [] # List of labels
+
+# Extract data, convert data type and load into
+
+with open(FILENAME, "r") as csvfile:
+    data = csv.DictReader(csvfile)
+    for row in data:
+
+        # Convert string with comma into float using convert_comma_separated_number() function
+
+        row["CTR - Cardiothoracic Ratio"] = convert_comma_separated_number(row["CTR - Cardiothoracic Ratio"])
+        row["Inscribed circle radius"] = convert_comma_separated_number(row["Inscribed circle radius"])
+        row["Heart perimeter"] = convert_comma_separated_number(row["Heart perimeter"])
+
+        row["ID"] = int(row["ID"])
+        row["Cardiomegaly"] = int(row["Cardiomegaly"])
+        row["Heart width"] = int(row["Heart width"])
+        row["Lung width"] = int(row["Lung width"])
+        row["Heart area"] = int(row["Heart area"])
+        row["Lung area"] = int(row["Lung area"])
+
+        row["xx"] = float(row["xx"])
+        row["yy"] = float(row["yy"])
+        row["xy"] = float(row["xy"])
+        row["normalized_diff"] = float(row["normalized_diff"])
+        row["Polygon Area Ratio"] = float(row["Polygon Area Ratio"])
+
+        label = row.pop(TARGET_COLUMN) # Store TARGET_COLUMN value in label variable
+        row.pop("ID")  # ID column does not store useful data
+
+        y_data.append(label) # Append label to the list of labels
+        X_data.append(row) # Append row to the list of samples
+
+print(X_data)
+print(y_data)
+
