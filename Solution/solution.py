48c13cdaa4cdd928bba8f6de3e3c23bb87828f84
Konrad Zatorski <kodiz2005@gmail.com>
2025-10-24 10:05:57 +0200

    Cross-Validation added, train_test_split replaced with StratifiedKFold to increase number of model tests and make the results more accurate for small amount of data. Improved LogicRegression metric with max_iter variable added. Zero division bug prevented in F1 and Precision scores. y variable now stores numpy array to enable list indexing in SKF loop.

--- Solution/solution.py
+++ Solution/solution.py
@@ -5,6 +5,7 @@ import csv
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 # Import Classical Machine Learning Methods
 from sklearn.tree import DecisionTreeClassifier
@@ -20,11 +21,16 @@ from sklearn.metrics import (
     recall_score,
     f1_score)
 
+# Import Cross-Validation feature 
+from sklearn.model_selection import StratifiedKFold
+
 # Constant variables
 FILENAME = "task_data.csv"
 TARGET_COLUMN = "Cardiomegaly"
 TEST_SIZE = 0.2
 RANDOM_STATE = 123  # To guarantee the same results for each program run
+MAX_ITER = 1000
+N_SPLITS = 5  # Number of CV tests (nr of folds)
 
 
 # Some strings contain comma instead of dot
@@ -69,51 +75,81 @@ with open(FILENAME, "r") as csvfile:
 vectorizer = DictVectorizer(sparse=False)  # Converts the list of dicts into a matrix.
 
 x = vectorizer.fit_transform(x_data_dicts)  # Ensures a fixed column order required by the model.
-y = y_data
-
-# Split data into training and test sets
-x_train, x_test, y_train, y_test = train_test_split(
-    x,
-    y,
-    test_size=TEST_SIZE,
-    random_state=RANDOM_STATE,
-    stratify=y
-    # To make sure that train and test splits have the same
-    # percentage of each class as the dataset, I use stratify=y.
-)
+y = np.array(y_data)
 
 # Scale features to similar range of value to use
 # other ML methods such as LogisticRegression,
 # KNeighborsClassifier and SVC
 
-scaler = StandardScaler()
-x_train = scaler.fit_transform(x_train)
-x_test = scaler.transform(x_test)
+SKF = StratifiedKFold(n_splits=N_SPLITS, shuffle=True,
+                      random_state=RANDOM_STATE)  # Shuffle data randomly before folding
 
-# Train some models
+models_result = {}
 
-DTC_model = DecisionTreeClassifier(random_state=RANDOM_STATE)
-RFC_model = RandomForestClassifier(random_state=RANDOM_STATE)
-KNC_model = KNeighborsClassifier()  # WITHOUT RANDOM_STATE
-SVC_model = SVC(random_state=RANDOM_STATE)
-LR_model = LogisticRegression(random_state=RANDOM_STATE)
+models = {
+    "DTC_model": DecisionTreeClassifier(random_state=RANDOM_STATE),
+    "RFC_model": RandomForestClassifier(random_state=RANDOM_STATE),
+    "KNC_model": KNeighborsClassifier(),  # WITHOUT RANDOM_STATE
+    "SVC_model": SVC(random_state=RANDOM_STATE),
+    "LR_model": LogisticRegression(random_state=RANDOM_STATE, max_iter=MAX_ITER),
+}
 
-for model in [DTC_model, RFC_model, KNC_model, SVC_model, LR_model]:
-    model.fit(x_train, y_train)
-    y_pred = model.predict(x_test)
+print(f"Starting Cross-Validation for {len(models)} models...")
+for model_name, model in models.items():
+    fold_scores = {"Accuracy Score": 0,
+                   "Precision Score": 0,
+                   "Recall Score": 0,
+                   "F1 Score": 0}
+    for train_index, test_index in SKF.split(x, y):
+        x_train = x[train_index]
+        x_test = x[test_index]
 
-    # Compare models efficiency using Classification Quality Metrics
-    acc_scr = accuracy_score(y_test, y_pred)
-    prec_scr = precision_score(y_test, y_pred)
-    rec_scr = recall_score(y_test, y_pred)
-    f1_scr = f1_score(y_test, y_pred)
+        y_train = y[train_index]
+        y_test = y[test_index]
 
-    print(f"""
-        --------------------------------------
-        ML Method: {model}
-        Accuracy score: {acc_scr}
-        Precision score: {prec_scr}
-        Recall score: {rec_scr}
-        F1 Score: {f1_scr}
-        --------------------------------------
-    """)
+        scaler = StandardScaler()
+
+        x_train_scaled = scaler.fit_transform(x_train)
+        x_test_scaled = scaler.transform(x_test)
+
+        model.fit(x_train_scaled, y_train)
+        y_pred = model.predict(x_test_scaled)
+
+        acc_scr = accuracy_score(y_test, y_pred)
+        prec_scr = precision_score(y_test, y_pred, zero_division=0)
+        rec_scr = recall_score(y_test, y_pred)
+        f1_scr = f1_score(y_test, y_pred, zero_division=0)
+
+        fold_scores["Accuracy Score"] += acc_scr
+        fold_scores["Precision Score"] += prec_scr
+        fold_scores["Recall Score"] += rec_scr
+        fold_scores["F1 Score"] += f1_scr
+
+    # Calculate results mean
+    for m, r in fold_scores.items():
+        r /= N_SPLITS
+        fold_scores[m] = r
+
+    models_result[model_name] = fold_scores
+
+print(models_result)
+
+# for model in [DTC_model, RFC_model, KNC_model, SVC_model, LR_model]:
+#     model.fit(x_train, y_train)
+#     y_pred = model.predict(x_test)
+
+#     # Compare models efficiency using Classification Quality Metrics
+#     acc_scr = accuracy_score(y_test, y_pred)
+#     prec_scr = precision_score(y_test, y_pred)
+#     rec_scr = recall_score(y_test, y_pred)
+#     f1_scr = f1_score(y_test, y_pred)
+
+#     print(f"""
+#         --------------------------------------
+#         ML Method: {model}
+#         Accuracy score: {acc_scr}
+#         Precision score: {prec_scr}
+#         Recall score: {rec_scr}
+#         F1 Score: {f1_scr}
+#         --------------------------------------
+#     """)
